e165e36f9c5a3c3f1216957b2fabfe1c3e0295e5
adipinhas <adi9247@gmail.com>
2020-07-22 14:05:38 +0300

    22.7

--- main.js
+++ main.js
@@ -16,7 +16,26 @@ var projects = [
                     isPhoneImg:true,
                     img:'./img/spot-it1.png',
                     des:'Play with your friend the popular cadrs game',
-                    codeUrl:'https://i-posts.web.app/#/'
+                    codeUrl:''
+                },
+                {
+                    name:'Make-It',
+                    url:'https://make-it-final-spring.herokuapp.com/',
+                    tech:'Vue.js, Vuex scss, node.js,',
+                    isPhoneImg:false,
+                    img:'https://i.ibb.co/r4Jp3kN/2020-06-01.png',
+                    des:'Plan',
+                    codeUrl:'https://github.com/Grape24/Final-Proj-MakeIt'
+                },
+                {
+                    name:'Weather',
+                    url:'https://adipinhas.github.io/weather-go/#/Tel%20Aviv/215854',
+                    tech:'React.js,css',
+                    isPhoneImg:true,
+                    img:'https://i.ibb.co/TP7r35G/weather123.jpg',
+                    des:'weather app',
+                    codeUrl:''
+
                 }
 
             ]
@@ -26,21 +45,19 @@ function renderProjects (projects) {
     var str = ''
     for(var i = 0;i<projects.length;i++){
        str+= `<div class="project">
-                            <div class="project-link">
-                                    <h4> ${projects[i].name}</h4><img ${projects[i].isPhoneImg && `class="project-img-phone"` } ${!projects[i].isPhoneImg && 'class="project-img-desktop"'} src="${projects[i].img}" alt=""> 
-                            </div>
-                            <div>
-                             Technologies: ${projects[i].tech}
-                            </div>
-                                ${projects[i].des}
-                                <div class="links">
-                                    <a class="link"  href="${projects[i].url}" >View Project</a>
-                                    <a class="link" href="${projects[i].codeUrl}">View code</a>
-                                </div>
-                        </div>
-
-
-       <div/>
+                    <div class="project-link">
+                        <h2> ${projects[i].name}</h2>
+                        <img ${projects[i].isPhoneImg && `class="project-img-phone"` } ${!projects[i].isPhoneImg && 'class="project-img-desktop"'} src="${projects[i].img}" alt=""> 
+                        ${projects[i].des}
+                    </div>
+                    <div>
+                        Technologies: ${projects[i].tech}
+                    </div>
+                    <div class="links">
+                        <a target="_blank" class="link"  href="${projects[i].url}" >View Project</a>
+                        <a target="_blank" class="link" href="${projects[i].codeUrl}">View code</a>
+                    </div>
+            </div>
        `
        
        
